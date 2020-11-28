b0471acd91f115dd53070b2a16e651171f7a0846
Afshin Mehrabani <afshin.meh@gmail.com>
2020-11-28 11:07:22 +0000

    initial commit

---
+++ docs/developers/changelog.md
@@ -0,0 +1,5 @@
+---
+title: Changelog
+---
+
+On [Release History](https://github.com/usablica/intro.js/blob/master/changelog.md) page you can find a complete list of releases history.
---
+++ sidebars.js
@@ -0,0 +1,21 @@
+module.exports = {
+  someSidebar: {
+    "Getting Started": [
+      'getting-started/welcome',
+      'getting-started/install',
+      'getting-started/start',
+      'getting-started/examples',
+    ],
+    Hints: [
+      'hints/api',
+      'hints/attributes',
+      'hints/options'
+    ],
+    Developers: [
+      'developers/build',
+      'developers/changelog'
+    ],
+
+  },
+};
+
