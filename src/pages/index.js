2eaac967a72ba3bfedd14d1feedb3a5a1267e64d
Afshin Mehrabani <afshin.meh@gmail.com>
2020-11-28 20:27:06 +0000

    feat: homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -439,6 +439,8 @@ function Home() {
 
         <Pricing />
 
+        <Customers />
+
         <Footer />
       </UserPreferencesProvider>
     </ThemeProvider>
@@ -756,6 +758,38 @@ function Pricing() {
       </div>
 
 
+    </div>
+  )
+}
+
+function Customers() {
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <p className="text-center text-base font-semibold uppercase text-gray-600 tracking-wider">
+          Over 5,000 businesses of all sizes trust us
+        </p>
+        <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/amazon.jpeg" alt="Amazon" />
+          </div>
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/nestle.jpg" alt="Nestle" />
+          </div>
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/sap.jpeg" alt="SAP" />
+          </div>
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/postat.png" alt="Postat" />
+          </div>
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/kobo.png" alt="Kobo" />
+          </div>
+          <div className="col-span-1 flex justify-center py-8 px-8 bg-gray-50">
+            <img className="max-h-12" src="/img/customers/federalbank.png" alt="FederalBank" />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
