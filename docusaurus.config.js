d56dcf3b6815d3710eace9eac4f1dff975739038
Afshin Mehrabani <afshin.meh@gmail.com>
2022-07-10 12:30:47 +0100

    feat: docusaurus upgrade

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,152 +1,170 @@
-const path = require('path');
+const path = require("path");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
-module.exports = {
-  title: 'Intro.js Docs',
-  tagline: 'User Onboarding and Product Walkthrough Library',
-  url: 'https://introjs.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'usablica',
-  projectName: 'introjs-website',
-  themeConfig: {
-    image: '/img/social.png',
-    algolia: {
-      appId: 'XNVVMJ2N5K',
-      apiKey: '5f24293aa1091a2b9f102052c4ebd9f1',
-      indexName: 'introjs',
-      algoliaOptions: {} // Optional, if provided by Algolia
-    },
-    navbar: {
-      hideOnScroll: true,
-      title: 'Intro.js',
-      logo: {
-        alt: 'Intro.js logo',
-        src: 'img/logo.svg',
-        href: 'https://introjs.com',
-        target: '_self'
-      },
-      items: [
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {
-          to: '/docs/examples/basic/hello-world',
-          label: 'Examples',
-          position: 'left'
-        },
-        {
-          to: 'https://github.com/usablica/intro.js/releases',
-          label: 'Download',
-          position: 'left'
-        },
-        {
-          to: '/docs/support',
-          label: 'Support',
-          position: 'left'
-        },
-        {
-          to: 'https://introjs.com/contact',
-          label: 'Contact us',
-          position: 'left',
-          target: '_blank'
-        },
-        {
-          to: 'https://introjs.com/blog',
-          label: 'Blog',
-          position: 'left'
-        },
-        {
-          href: 'https://github.com/usablica/intro.js',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Intro.js',
-              to: 'docs/',
-            },
-            {
-              label: 'Install',
-              to: 'docs/getting-started/install',
-            },
-            {
-              label: 'Quick Start',
-              to: 'docs/getting-started/start',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/intro.js',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/usablica',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Intro.js Blog',
-              to: 'https://introjs.com/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/usablica/intro.js',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Usablica`,
-    },
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: "Intro.js Docs",
+  tagline: "User Onboarding and Product Walkthrough Library",
+  url: "https://introjs.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "usablica",
+  projectName: "introjs-website",
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
   },
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      image: "/img/social.png",
+      algolia: {
+        appId: "XNVVMJ2N5K",
+        apiKey: "5f24293aa1091a2b9f102052c4ebd9f1",
+        indexName: "introjs",
+        algoliaOptions: {}, // Optional, if provided by Algolia
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+      navbar: {
+        hideOnScroll: true,
+        title: "Intro.js",
+        logo: {
+          alt: "Intro.js logo",
+          src: "img/logo.svg",
+          href: "https://introjs.com",
+          target: "_self",
+        },
+        items: [
+          {
+            to: "docs/",
+            activeBasePath: "docs",
+            label: "Docs",
+            position: "left",
+          },
+          {
+            to: "/docs/examples/basic/hello-world",
+            label: "Examples",
+            position: "left",
+          },
+          {
+            to: "https://github.com/usablica/intro.js/releases",
+            label: "Download",
+            position: "left",
+          },
+          {
+            to: "/docs/support",
+            label: "Support",
+            position: "left",
+          },
+          {
+            to: "https://introjs.com/contact",
+            label: "Contact us",
+            position: "left",
+            target: "_blank",
+          },
+          {
+            to: "https://introjs.com/blog",
+            label: "Blog",
+            position: "left",
+          },
+          {
+            href: "https://github.com/usablica/intro.js",
+            label: "GitHub",
+            position: "right",
+          },
+        ],
+      },
+      footer: {
+        style: "dark",
+        links: [
+          {
+            title: "Docs",
+            items: [
+              {
+                label: "Intro.js",
+                to: "docs/",
+              },
+              {
+                label: "Install",
+                to: "docs/getting-started/install",
+              },
+              {
+                label: "Quick Start",
+                to: "docs/getting-started/start",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/intro.js",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/usablica",
+              },
+            ],
+          },
+          {
+            title: "More",
+            items: [
+              {
+                label: "Intro.js Blog",
+                to: "https://introjs.com/blog",
+              },
+              {
+                label: "GitHub",
+                href: "https://github.com/usablica/intro.js",
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} Usablica`,
+      },
+    }),
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      "classic",
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         sitemap: {
-          changefreq: 'weekly',
+          changefreq: "weekly",
           priority: 0.5,
         },
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           showLastUpdateAuthor: false,
           showLastUpdateTime: false,
-          editUrl: 'https://github.com/usablica/introjs-website/edit/master/',
+          editUrl: "https://github.com/usablica/introjs-website/edit/master/",
         },
-	blog: false,
+        blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
+      }),
+    ],
+  ],
+  themes: [require.resolve("@docusaurus/theme-live-codeblock")],
+  plugins: [
+    path.resolve(__dirname, "./plugins/jsdom"),
+    path.resolve(__dirname, "./plugins/tailwind"),
+    [
+      "@docusaurus/plugin-google-analytics",
+      {
+        trackingID: "UA-69239984-1",
       },
     ],
   ],
-  themes: [require.resolve('@docusaurus/theme-live-codeblock')],
-  plugins: [
-    path.resolve(__dirname, './plugins/jsdom'),
-    path.resolve(__dirname, './plugins/tailwind'),
-    [
-      '@docusaurus/plugin-google-analytics',
-      {
-        trackingID: 'UA-69239984-1'
-      },
-    ],
-  ]
 };
+
+module.exports = config;
